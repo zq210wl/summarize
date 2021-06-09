f366427d2da0ac4c4e79f363db4b1916b44acc09
zhangqisteven <zhangqisteven@didiglobal.com>
2021-06-09 11:23:15 +0800

    update

--- summarize/css相关.js
+++ summarize/css相关.js
@@ -32,6 +32,36 @@
 
 
 /*
+ 【移动端适配相关知识】
+  * 物理像素、逻辑像素、物理像素/逻辑像素PPR
+    * 物理像素是指设备屏幕的物理像素，一个物理像素点是由四个点构成的
+  * document.documentElement.clientWidth（文档默认宽度、布局视口）
+     * 很多手机的文档默认宽度是980px，这样的宽度会导致部门文档内容超出屏幕，需要滚动来显示
+  * device-width（逻辑像素宽度、视觉视口宽度、设备宽度、屏幕宽度window.screen.width） 
+    * 不同手机的device-width不一样，比如有：iphone5:320px，iphoneX:375px，ipad:768px
+  * <meta name="viewport" content="width=device-width"></meta>
+    * 就是设置 document.documentElement.clientWidth = device-width
+  * 移动端适配方案: 整体等比缩放，有下面三种方式：
+    * 整体viewport等比缩放、就按照真实的设计稿宽度来开发、<meta name="viewport" content="width=device-width, initial-scale=[device-width/开发时的设计稿参照宽度]"></meta>
+    * rem、<html style="font-size: 100px;">、device-width/开发时的设计稿参照宽度
+    * vw
+  * 响应式高清图片适配
+    * 我通常所说的是【图片尺寸】指的是 【物理像素】 
+    * 我通常所说的是【图片分辨率】指的是 【单位长度的物理像素】
+    * DPR 即window.devicePixelRatio，这个值在出场的时候就固定了，不能修改（这个值跟屏幕分辨路有关，如果非要修改，可以调整屏幕分辨率）
+      * DPR = 2，说明1个逻辑像素 == 2个物理像素 
+    * 假如屏幕上需要显示一个<img style="width:30px"/>的图片：
+      * DPR=1的屏幕需要一个宽度至少为：1 * 30 物理像素的图片 
+      * DPR=2的屏幕需要一个宽度至少为：2 * 30 物理像素的图片
+      * DPR=3的屏幕需要一个宽度至少为：3 * 30 物理像素的图片
+    * 实现方法：
+      * 媒体查询
+      * <img src='aa.jpg' srcset="aa1.jpg 1x, aa2.jpg 2x, aa3.png 3x" /> 
+      * <img src='aa.jpg' srcset="aa1.jpg 370w, aa2.jpg 800w, aa3.png 960w" /> 
+*/
+
+
+/*
  【各种基础】
   * box-sizing: border-box
 */
