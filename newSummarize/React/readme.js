6f8b0be1c31290748f61068faee20ea83f155e13
imac <imac@imacdeMacBook-Pro.local>
2023-11-10 15:21:02 +0800

    add new

--- newSummarize/React/readme.js
+++ newSummarize/React/readme.js
@@ -4,6 +4,8 @@
     - https://segmentfault.com/a/1190000039101758
   - React commit阶段详解
     - https://segmentfault.com/a/1190000039097269
+  - React源码解析之优先级Lane模型
+    - https://juejin.cn/post/7008802041602506765
 */
 
 /*
@@ -22,6 +24,44 @@
   - Reconciler（协调器）—— 负责找出变化的组件，这个时候会执行render方法来绘制fiber节点
   - Renderer（渲染器）—— 负责将变化的组件渲染到页面上，也就是所谓的commit阶段，
                        各个生命周期的回调函数也是在commit阶段被触发的
+
+- React中有三套优先级机制：
+  - Lane优先级
+    - 31种
+  - React事件优先级
+    - // 离散事件优先级，例如：点击事件，input输入等触发的更新任务，优先级最高
+      export const DiscreteEventPriority: EventPriority = SyncLane;
+    - // 连续事件优先级，例如：滚动事件，拖动事件等，连续触发的事件
+      export const ContinuousEventPriority: EventPriority = InputContinuousLane;
+    - // 默认事件优先级，例如：setTimeout触发的更新任务
+      export const DefaultEventPriority: EventPriority = DefaultLane;
+    - // 闲置事件优先级，优先级最低
+      export const IdleEventPriority: EventPriority = IdleLane;
+  - Scheduler优先级
+    - export const NoPriority = 0; //没有优先级
+    - export const ImmediatePriority = 1; // 立即执行任务的优先级，级别最高
+    - export const UserBlockingPriority = 2; // 用户阻塞的优先级
+    - export const NormalPriority = 3; // 正常优先级
+    - export const LowPriority = 4; // 较低的优先级
+    - export const IdlePriority = 5; // 优先级最低，闲表示任务可以闲置
+
+- React优先级转换：
+  - React事件优先级 -> Lane优先级 -> Scheduler优先级
+
+- Scheduler的时间分片
+  - 在render阶段，每次遍历前，都会通过Scheduler提供的shouldYield方法判断是否需要中断遍历
+    function workLoopConcurrent() {
+        // Perform work until Scheduler asks us to yield
+        while (workInProgress !== null && !shouldYield()) {
+            performUnitOfWork(workInProgress);
+        }
+    }
+  - 在Schdeduler中，为任务分配的初始剩余时间为5ms
+
+- Scheduler的timerQueue和taskQueue为什么都使用小顶堆来实现？
+  - 可以最快的取出队列中时间最早的那个元素，时间复杂度O(1)
+  - 取出元素之后可以很快的进行剩余元素的排序，时间复杂度为O(nlogn)
+  - 插入任务的时候可以很快的根据任务的时间把它插入到对应的位置，时间复杂度为O(nlogn)
 
 - Fiber的这种链表结构，是一种可以中断并在此开启的【递归调用】的数据结构
 
@@ -71,14 +111,52 @@
   - React的状态更新分为同步和异步更新，但是它们都会对当前代码之前的state进行合并
     - 同步任务会执行 performSyncWorkOnRoot、Scheduler的scheduleSyncCallback
     - 异步任务会执行 performConcurrentWorkOnRoot、Scheduler的lanePriorityToSchedulerPriority
-  - 异步更新：state会被放在一个队列里，最后合并执行
+  - 异步更新：state会被转成一个Update对象，以链表形式存在放在当前fiber节点的updateQuenen中，最后合并执行
   - 针对基于 ReactDom.createRoot() 创建出来的组件，同步和异步任务都会进行合并，不区分是否为react控制下的更新
   - 只有相同优先级的的state变化才会被合并执行，低优先级的更新会被放在下一次render中执行
 
+- commit的三个阶段
+  - Before mutation
+    - 触发的生命周期：
+      - useEffect (异步调用)
+  - Mutation
+    - 执行dom的增删改
+    - 更新ref，但是还没有执行ref回调来真正的设置
+    - 触发的生命周期：
+      - useLayoutEffect的销毁回调
+  - Layout
+    - 到这个阶段dom已经更新完毕了，由于 JS 的同步执行阻塞了主线程，所以此时 JS 已经可以获取到新的DOM，但是浏览器对新的DOM并没有完成渲染
+    - 触发的生命周期：
+      - componentDidMount (同步调用)
+      - componentDidUpdate (同步调用)
+      - useLayoutEffect (同步调用)
+      - this.setState({ xxx: 1 }, () => {
+           console.log("这个方法被调用了");
+        }); (同步调用)
+      - 真正的执行ref回调来设置ref (同步调用)
+
+- diff算法
+  - 在我们做数组相关的算法题时，经常使用双指针从数组头和尾同时遍历以提高效率，但是这里却不行
+    - 因为同级的Fiber节点是由sibling指针链接形成的单链表，即不支持双指针遍历
+  - 用到了key:value的map结构来转换旧节点数组
+  - 从 abcd 变为 dabc，只需要将d移动到前面，但实际上React保持d不变，将abc分别移动到了d的后面
+   - 从这点可以看出，考虑性能，我们要尽量减少将节点从后面移动到前面的操作
+  - 具体算法：https://react.iamkasong.com/diff/multi.html#diff%E7%9A%84%E6%80%9D%E8%B7%AF
+
 - React任务优先级
-  - 同步任务
-  - 高优先级任务
-  - 低优先级任务
+  - 同步任务：生命周期方法、受控的用户输入（比如输入框内输入文字）
+  - 高优先级任务：交互事件（比如动画）
+  - 低优先级任务：其他（比如数据请求）
+
+- React各种模式最终体现在fiber.mode值的不同，流程都差不多，只是一些细节会不同
+
+- React的hooks也是用链表实现的一个hooks链表，用来记录每个hooks的位置
+
+- React.forwardRef(function(props, ref){  })
+  - 子组件用forwardRef来包裹
+  - 可以让父组件在给子组件设置了ref属性的时候，可以获取到子组件想要暴露给父组件的dom元素
+
+- Suspense、useDeferredValue、useTransition  - TODO
 */
 
 /*
---
+++ newSummarize/Webpack/readme.js
@@ -0,0 +1,151 @@
+/*
+- 模块化历程
+  - commonjs规范
+    - nodejs内置的模块化管理规范，特点是：全部文件都是nodejs启动时同步一次加载完
+    - modejs的8.5版本以后开始内置支持ES6模块的加载规范了，之前的版本也可以用Babel来编译执行
+    - 浏览器端也有一个模块化的库实现了：browserify
+    - API使用方式：
+      const a = require('moduleA');
+      const b = require('moduleB');
+      exports.xx = 1;
+      exports.yy = 2;
+      或者：
+      module.exports = {
+        xx: 1,
+        yy: 2,
+      };
+  - AMD规范
+    - 浏览器端的异步模块化加载规范
+    - 模块化库：requirejs
+    - API使用方式：
+      // 定义无依赖的模块
+      define(function(){
+        const a = 1;
+        return { a };
+      })；
+      // 定义有依赖的模块
+      define([moduleA, moduleB], function(a, b){
+        const x = a + 1;
+        const y = b + 2;
+        return { x, y };
+      })；
+      // 定义有模块名字的模块
+      define('moduleName', [moduleA, moduleB], function(a, b){
+        const x = a + 1;
+        const y = b + 2;
+        return { x, y };
+      })；
+      // 引用模块
+      requirejs(['moduleA', 'moduleB'], function(a, b) {
+        console.log(a + b);
+      });
+  - CMD模块化规范
+    - commonjs + requirejs，支持用到的时候再异步加载，也支持同步加载
+    - 模块化库：sea.js
+    - API使用方式：
+      define(function(require, exports, module){
+        let abc = 1;
+        // 同步引入
+        let moduleX = require('./moduleX');
+        moduleX();
+        // 异步引入
+        require.async('./moduleY', function(moduleY){
+          abc = moduleY.say();
+        });
+        module.exports = { x: abc };
+      });
+  - ES6模块化规范
+    - 需要用到Babel来编译，
+    - 相关的模块化库：webpack
+    - API使用方式：
+      import xx from './moduleX';
+      export { a: xx };
+      export default { b: yy };
+    - 浏览器内置加载方式：
+      <script type="module" src="https://www.xx.com/a.js"></script>
+      <script type="module">
+        var xx = 1;
+        console.log(xx);
+      </script>
+*/
+
+/*
+- ES6 中的 export 和 export default
+  - export { x, y };
+    - 这是固定用法，导出的值必须放到{}中
+  - export default { x, y };
+    或
+    let a = { x, y };
+    export defualt a;
+    或
+    let x = 1;
+    export default x;
+    - 这是默认导出一个对象字面量
+  - 特别说明：
+    - 上面两种方式导出的都是对变量的引用，不是拷贝；
+    - 导出的变量是【不能重新赋值】的，但是变量如果是对象的话，可以修改对象已经存在的字段的值； 
+
+- ES6 中引入模块的路径
+  - import { name } from 'moduleX.js'; // 引入第三方库，会从 node_modules 目录中去找
+  - import { name } from './moduleX.js'; // 相对路径的引用方式
+  - import { name } from '/a/b/moduleX.js'; // 绝对路径的引用方式
+  - import { name } from 'http://a/b/moduleX.js'; // 加载网络路径的引用方式
+
+- ES6 动态加载模块
+  - import('./moduleX.js').then(function(moduleX){ console.log(moduleX); });
+
+- ES6 导出 default 默认成员的两种方法：
+  - import xx, { name } from 'moduleX.js';
+  - import { name, default as xx } from 'moduleX.js';
+
+- ES6 引用并直接导出：
+  - export { name, age, default as xx } from 'moduleX.js'; // 只需要把import直接换成export就行了
+*/
+
+/*
+- webpack
+  - mode模式：production、development、none
+  - loader是用来加载不同类型的资源，根据不同的规则来看看以什么样的方式来把它编译成【js代码】加载进来
+    - 分类：
+      - 编译转换类，如：babel-loader
+      - 文件操作类，如：file-loader、url-loader、css-loader
+      - 代码检查类，如：eslint-loader
+    - 原理：
+      - 多个loader其实就是一个管道的概念，A-loader -> B-loader -> C-loader
+  - output.publicPath = 'dist/'; // 设置资源导出的目录（也就是网站的根目录）
+  - 核心原理
+    - 从入口文件开始找到所有的依赖文件，生成一个文件依赖树，
+    - 然后再把这些不同的文件给到不同的loader来处理，
+    - 最后处理完后把最终的代码放到对应的bundle文件中。
+  - 插件机制 Plugin
+    - 作用：除了资源加载以外的其它自动化的工作，例如：
+      - 自动清除 dist 目录
+      - 自动拷贝不需要参与打包静态文件到输出目录
+      - 压缩输出代码
+      - 自动生成引用好资源文件的html文件
+      - 热更新
+    - 工作机制：
+      - 在webpack编译的过程中会在不同阶段有一些对应的事件钩子hooks，也就是生命周期钩子，
+        插件可以在这些这些hooks中挂载对应的处理逻辑，
+        等webpack执行到对应的hooks的时候，就会执行注入的处理逻辑
+  - HMR 热更新
+    - 并不可以开箱即用，需要使用HMR提供的API来手动添加对应的处理代码
+    - 针对不同的JS没有通用的处理方案
+  - DefinePlugin
+    - 往代码中注入全局变量，比如：
+      new webpack.DefinePlugin({
+        NODE_ENV: '"PRODUCTION"',
+      })
+  - TreeShaking 只对 ES module 生效，还需要满足以下两个条件才会起作用：
+    - webpack配置需要开启treeShaking功能
+    - 被用到的库的 package.json 中 sideEffects: false 的代码库会被进行treeShaking处理，
+      或者直接指定哪些文件有副作用，不要进行treeShaking，
+      如：sideEffects: ['./aa.js', './dd.css']
+  - 代码分离 Code Splitting，以下两个方式都会生成多个chunk：
+    - 多入口打包
+    - 动态导入，import().then()
+  - optimization 配置项专门用来配置压缩之类的优化项
+  - 文件缓存的设置，需要用到[name]-[hash]的形式
+    - chunkHash - 有可能js文件和css文件都来自同一个chunk，这个时候如果只改变css内容，那么生成的js文件名也会变化
+    - contentHash - 这是【推荐使用】的，真正的文件内容发生变化才会改变
+*/
